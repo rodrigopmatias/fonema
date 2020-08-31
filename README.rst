7815d6087248d18d8ffafb3509ffe284e9da65a1
Rodrigo Pinheiro Matias <rodrigopmatias@gmail.com>
2020-08-31 14:21:52 -0300

    foram corrigidos alguns detalhes da documentação.

---
+++ README.rst
@@ -0,0 +1,4 @@
+Importante
+----------
+
+Baseado no repositório https://github.com/carlosjordao/metaphone-ptbr de autoria do Carlos Costa Jordão.
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""
+'''
    Copyright (C) 2017  Bruno Guberfain do Amaral <bruno.do.amaral@gmail.com>
 
    Released under the terms of the BSD license
@@ -13,7 +13,7 @@
        other materials provided with the distribution.
   
   
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND
     ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
     DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
@@ -25,23 +25,25 @@
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   
   ***********************************************************************
-"""
+'''
 import os
 
 from setuptools import setup, Extension
 
 c_ext = Extension(
-   "fonema", 
+   'fonema', 
    [
-      "metaphone_ptbrpy.c",
-      os.path.join("src", "metaphone_ptbr.c"),
+      'metaphone_ptbrpy.c',
+      os.path.join('src', 'metaphone_ptbr.c'),
    ]
 )
 
 setup(
     name='fonema',
-    version='1.0.5',
+    author='Rodrigo Pinheiro Matias',
+    author_email='rodrigopmatias@gmail.com',
+    version='1.0.6',
     ext_modules=[c_ext],
-    include_dirs=[".", "src"]
-    
+    include_dirs=['.', 'src'],
+    url=''
 )
